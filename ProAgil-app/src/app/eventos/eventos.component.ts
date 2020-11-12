822feee5a16b60b80f4691e91cc4b86aaa49fbd2
karin-jpg <andradkarin@gmail.com>
2020-11-12 20:34:23 -0300

    Adicionado um filtro dinamico para os eventos

--- ProAgil-app/src/app/eventos/eventos.component.ts
+++ ProAgil-app/src/app/eventos/eventos.component.ts
@@ -9,16 +9,36 @@ import { Component, OnInit } from '@angular/core';
 
 export class EventosComponent implements OnInit {
 
+  // tslint:disable-next-line: variable-name
+  _filtroLista: string;
+
+  get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  eventosFiltrados: any = [];
   eventos: any = [];
   imagemLargura = 50;
   iamgemMargem = 50;
   mostrarImagem = false;
-  filtroLista = '';
+
   constructor(private http: HttpClient) { }
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
     this.getEventos();
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
   }
 
   // tslint:disable-next-line: typedef
